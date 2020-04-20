f04844b8363a4d4c26ca6fe3c851aa40c3846ba3
Thomas Davis <ThomasDavisA@gmail.com>
2020-04-20 15:16:27 -0700

    additional error handling for types

--- main.js
+++ main.js
@@ -10,12 +10,16 @@ function createGreeting(name, age) {
     if (!age || !name) {
         throw new Error('Argument is not valid.');
     }
+
+    if (!Number.isNaN(age) || typeof name !== 'string') {
+        throw new TypeError();
+    }
     const yearOfBirth = getYearOfBirth(age);
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 try {
-    const greeting1 = createGreeting('Roland', );
+    const greeting1 = createGreeting('Roland', '29');
     console.log(greeting1);
 } catch(error) {
     console.log(error);
