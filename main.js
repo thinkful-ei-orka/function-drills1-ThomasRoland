4d646cd0cd3fc2d74faa7d5ae8afb657d80fdcd7
Thomas Davis <ThomasDavisA@gmail.com>
2020-04-20 14:58:06 -0700

    added error handling

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 function getYearOfBirth(age) {
+    if(age < 0) {
+        throw new Error('Age can not be negative!');
+    }
     return 2020 - age;
 }
 function createGreeting(name, age) {
@@ -8,5 +11,9 @@ function createGreeting(name, age) {
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
-const greeting1 = createGreeting('Roland', 31);
-console.log(greeting1);
+try {
+    const greeting1 = createGreeting('Roland', 31);
+    console.log(greeting1);
+} catch(error) {
+    console.log(error);
+}
