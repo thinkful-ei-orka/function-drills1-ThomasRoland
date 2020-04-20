4072ecd50efd0284c2017f72fab78ec82acc46c3
Roland Pleasant <rolandpleasant@gmail.com>
2020-04-20 16:05:53 -0600

    more error handling

--- main.js
+++ main.js
@@ -7,12 +7,15 @@ function getYearOfBirth(age) {
     return 2020 - age;
 }
 function createGreeting(name, age) {
+    if (!age || !name) {
+        throw new Error('Argument is not valid.');
+    }
     const yearOfBirth = getYearOfBirth(age);
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 try {
-    const greeting1 = createGreeting('Roland', 31);
+    const greeting1 = createGreeting('Roland', );
     console.log(greeting1);
 } catch(error) {
     console.log(error);
